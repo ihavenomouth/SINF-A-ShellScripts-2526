0b3473e2191b711b37fb44aca6d58525dd5d405e
Javier <javmanfer@gmail.com>
2026-02-10 21:37:50 +0100

    Subida solución del virtualvox

--- 7_Funciones/funciones.sh
+++ 7_Funciones/funciones.sh
@@ -22,6 +22,18 @@
 # saludo
 
 
+# Debemos entender la función como si fuera una orden local al script, por eso se llama como 
+# si fuera una orden.
+
+# Nota: si defino una función que se llama como una orden que ya existe en el sistema, la función
+# tiene prioridad. Por ejemplo abajo cuando haga Bash ejecutará lo primero que encuentre y la
+# función está definida en el propio script mientras que la orden habrá que buscarla en el $PATH
+
+# function ls(){
+#   echo "se ejecuta la función ls, no la orden ls"
+# }
+# ls
+
 # -----------------------------------------------------
 # Declaración y llamada de una función sin parámetros
 # -----------------------------------------------------
@@ -54,16 +66,56 @@
 # Devolver una valor por parte de una función
 # -----------------------------------------------------
 
-function sumar(){
-  sumando1=$1   # el primer parámetro
-  sumando2=$2   # el segundo parámetro
-  # Lo que imprimiría por pantalla es el resultado
-  echo $(( $sumando1 + $sumando2 ))
-  # echo "Fin de la función"
-  # date
+# function sumar(){
+#   sumando1=$1   # el primer parámetro
+#   sumando2=$2   # el segundo parámetro
+#   # Lo que imprimiría por pantalla es el resultado
+#   echo $(( $sumando1 + $sumando2 ))
+#   # echo "Fin de la función"
+#   # date
+# }
+
+# resultado=$( sumar 45 67 )
+
+# echo "El resultado es $resultado"
+
+
+# -----------------------------------------------------
+# Variables globales y locales
+# -----------------------------------------------------
+
+
+# Variables globales:
+# ---------------------
+function variable_global(){
+  # a es una variable global
+  echo $a      # "letra A"
+  a="Cambiamos el valor de a"
+  b="letra B"  # esta también es una variable global
 }
 
-resultado=$( sumar 45 67 )
 
-echo "El resultado es $resultado"
+a="letra A"    # a es una variable global, disponible en todo el script
+
+variable_global
+
+#tanto a como b existen en todo el script
+echo $a        # "Cambiamos el valor de a"
+echo $b        # "letra B"
+
+
+# Variables locales:
+# ---------------------
+
+function variable_local(){
+  # las variables definidas como local en una función sólo existen dentro de esa función
+  local c="letra C"
+  echo "Valor de c: $c" # Valor de c: letra C
+}
+
+variable_local
+
+# c no existe fuera de la función
+echo "Valor de c: $c" # Valor de c:
+
 
--- 7_Funciones/virtualbox.sh
+++ 7_Funciones/virtualbox.sh
@@ -1,22 +1,77 @@
-Virtualbox
+#!/bin/bash
 
-Crea un script que reciba uno o tres parámetros.
+# Virtualbox
+# Crea un script que reciba uno o tres parámetros.
+# Si no recibe ninguno, dos o más de tres mostrará un mensaje de error y terminará.
 
-Si no recibe ninguno, dos o más de tres mostrará un mensaje de error y terminará.
 
-Si el primer parámetro es --help, se mostrará la ayuda y terminará.
+function errorParametros(){
+  echo "Error: Nº de parámetros incorrecto" 1>&2
+  echo "Debe indicar uno o tres parámetros" 1>&2
+  exit 1
+}
 
-Si el primer parámetro es lista se ejecutará lo siguiente y terminará:
-vboxmanage list runningvms
 
-Si sólo se recibe un parámetro y no es ninguno de esos dos, se mostrará un mensaje de error y terminará.
+# if [ $# -eq 0 ];then
+#   error
+# fi
 
-Si se reciben tres parámetros, el primer parámetro tiene que ser limitar. En caso contrario se mostrará un mensaje de error y terminará.
+[ $# -eq 0 ] && errorParametros
+[ $# -eq 2 ] && errorParametros
+[ $# -gt 3 ] && errorParametros
 
-Si se reciben tres parámetros y el primer parámetro es limitar, se ejecutará una orden que limite al número que se pasa como segundo parámetro el uso de la CPU que puede hacer la máquina virtual cuyo id se pasa como tercer parámetro.
 
-Por ejemplo:
-./script limitar 70 3d79ec34-5a0b-481c-8661-51f85d9db585
 
-Ejecutará:
-VBoxManage controlvm 3d79ec34-5a0b-481c-8661-51f85d9db585 cpuexecutioncap 70
+
+# Si el primer parámetro es --help, se mostrará la ayuda y terminará.
+if [ "$1" == "--help" ];then
+  echo "Gestiona máquinas virtuales de Virtualbox"
+  echo
+  echo "Uso: $0 [--help | lista]"
+  echo "Uso: $0 limitar executioncap uuid"
+  echo
+  echo "Parámetros:"
+  echo "   --help       : muestra esta ayuda"
+  echo "   lista        : muestra una lista con las máquinas virtuales que se están ejecutando"
+  echo "   executioncap : %de CPU máximo que puede usar una máquina virtual"
+  echo "   uuid         : identificado de la máquina virtual"
+  echo
+  echo "Ejemplo:"
+  echo "$0 limitar 70 3d79ec34-5a0b-481c-8661-51f85d9db585"
+  exit 0
+fi
+
+
+# Si el primer parámetro es lista se ejecutará lo siguiente y terminará:
+if [ "$1" == "lista" ];then
+  vboxmanage list runningvms
+  exit 0
+fi
+
+
+# Si sólo se recibe un parámetro y no es ninguno de esos dos, se mostrará un mensaje de error y terminará.
+
+# Si se llega a esta línea es que hay uno o tres parámetros y el primero no es ni --help ni lista.
+if [ $# -eq 1 ];then
+  echo "Si sólo indica un parámetro, los únicos válidos son --help o lista" 1>&2
+  exit 2
+fi
+
+
+# Si se reciben tres parámetros, el primer parámetro tiene que ser limitar. En caso contrario se mostrará un mensaje de error y terminará.
+if [ $# -eq 3 ] && [ "$1" != "limitar" ];then
+  echo "Si se indicab tres parámetros, el primero debe ser limitar" 1>&2
+  exit 3
+fi
+
+# Si llego aquí es porque hay tres parámetros y el primero es limitar
+
+
+# Si se reciben tres parámetros y el primer parámetro es limitar, se ejecutará una orden que limite al número que se pasa como segundo parámetro el uso de la CPU que puede hacer la máquina virtual cuyo id se pasa como tercer parámetro.
+
+# Por ejemplo:
+# ./script limitar 70 3d79ec34-5a0b-481c-8661-51f85d9db585
+
+# Ejecutará:
+# # VBoxManage controlvm 3d79ec34-5a0b-481c-8661-51f85d9db585 cpuexecutioncap 70
+VBoxManage controlvm $3 cpuexecutioncap $2
